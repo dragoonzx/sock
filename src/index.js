af59b486a1edfd35ff6c721626010e3757d242da
dragoonzx <dragoonz@yandex.ru>
2019-03-23 20:06:01 +0300

    keek

---
+++ src/index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile('C:\\Users\\xiaomi\\sockets\\public\\index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.on('eventServer', function (data) {
+		console.log(data);
+		socket.emit('eventClient', { data: 'Hello Client' });
+	});
+	socket.on('disconnect', function () {
+		console.log('user disconnected');
+	});
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
